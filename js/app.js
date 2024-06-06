a11bf40fca1c9511477442a348cbda4d3b042d52
DVs07 <aldopablo7@hotmail.com>
2024-06-05 22:57:29 -0300

    Se agrego la funcionalidad de validar los datos ingresados en el formulario, se muestra un mensaje a través de la consola del navegador.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ const emailInput = document.querySelector('#email');
 const fechaInput = document.querySelector('#fecha');
 const sintomasInput = document.querySelector('#sintomas');
 
+const formulario = document.querySelector('#formulario-cita');
+
 // Objeto de cita
 const citaObj ={
     mascota: '',
@@ -17,19 +19,38 @@ console.log(citaObj);
 
 // Eventos
 nombreMascotaInput.addEventListener('input', datosCita);
-
 propietarioInput.addEventListener('input', datosCita);
-
 emailInput.addEventListener('input', datosCita);
-
 fechaInput.addEventListener('input', datosCita);
-
 sintomasInput.addEventListener('input', datosCita);
+
+formulario.addEventListener('submit', crearCita);
 
 
 function datosCita(e) {
     citaObj[e.target.name] = e.target.value;
     console.log(citaObj);
+}
+
+function crearCita(e) {
+    e.preventDefault();
+
+    // console.log("Creando cita...");
+    // Validacion
+    // const {mascota, propietario, email, fecha, sintomas} = citaObj;
+    // if(mascota.trim() === '' || propietario.trim() === '' || email.trim() === '' || fecha.trim() === '' || sintomas.trim() === '') // trim() elimina los espacios en Blanco.
+    // {
+    //     console.log('Todos los campos son obligatorios');
+    //     return;
+    // }
+
+    //  Alternativa de validacion mas corta.
+    if(Object.values(citaObj).some(input => input.trim() === '')){
+        console.log('Todos los campos son obligatorios');
+        return;
+    }
+
+    console.log("Afuera del if");
 }
 
 
